50b33231b9d3943029a4dcc9c349f02e710de19d
Johan Lund Neergaard <johanlundn@gmail.com>
2025-06-27 10:21:25 +0200

    tests and dynamically reduce max timeout
    global options
    
    spelling
    
    dynamically update timeout
    
    fix variable name

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,7 @@ export const RETRY_ATTEMPTS = 4
 export const RETRY_TIMEOUT = 500
 // The exponential growth factor which the timeout is multiplied by after each failed attempt.
 export const RETRY_GROWTH_FACTOR = 2
-// The response status codes where we a retry should be attempted
+// The response status codes where a retry should be attempted.
 export const RETRY_STATUS_CODES = [408]
+// The base timeout should dynamically update based on the average response time.
+export const RETRY_DYNAMTIC_TIMEOUT = true
---
+++ test/integration/dynamicTimeout.test.js
@@ -0,0 +1,79 @@
+import { describe, it, beforeEach } from 'node:test'
+import assert from 'node:assert'
+import nock from 'nock'
+import { retryOptions, fetchWithRetry } from '../../src/retry.js'
+
+const url = 'http://example.com'
+
+const mockCall = (delay = 0, count = 1) => {
+  return nock(url)
+    .get('/')
+    .delay(delay)
+    .times(count)
+    .reply(200, {})
+}
+
+describe('dynamic timeout tests', async () => {
+  beforeEach(() => nock.cleanAll())
+
+  it('Check that the dynamic timeout correctly increases the base timeout duration', async () => {
+    retryOptions.timeout = 50
+    mockCall(1000, 100)
+    try {
+      const response = await fetchWithRetry(url,
+        {
+          retries: 100,
+          growthFactor: 2
+        }
+      )
+      assert.equal(retryOptions.timeout, 1600)
+    } catch (error) {
+      assert.fail(error.message)
+    }
+  })
+
+  it('Check that the dynamic timeout correctly reduces the base timeout duration', async () => {
+    retryOptions.timeout = 1600
+    try {
+      mockCall(50, 50)
+      for(let i = 0; i < 50; i++) {
+        const response = await fetchWithRetry(url,
+          {
+            retries: 1,
+            growthFactor: 2
+          }
+        )
+      }
+      assert.equal(retryOptions.timeout, 284.765625)
+    } catch (error) {
+      assert.fail(error.message)
+    }
+  })
+
+  it('Check that the dynamic timeout correctly increases and reduces the base timeout duration', async () => {
+    retryOptions.timeout = 50
+    mockCall(2000, 50)
+    try {
+      const response = await fetchWithRetry(url,
+        {
+          retries: 50,
+          growthFactor: 2
+        }
+      )
+      assert.equal(retryOptions.timeout, 400)
+      nock.cleanAll()
+      mockCall(0, 50)
+      for(let i = 0; i < 50; i++) {
+        const response = await fetchWithRetry(url,
+          {
+            retries: 1,
+            growthFactor: 2
+          }
+        )
+      }
+      assert.equal(retryOptions.timeout, 300)
+    } catch (error) {
+      assert.fail(error.message)
+    }
+  })
+})
